79311b282188d53e0b159b09c5d5738db464ed50
Sean Low <seanlow22@gmail.com>
2024-06-16 23:16:09 +0800

    Patching Array (Greedy)

--- hard/patching_array.py
+++ hard/patching_array.py
@@ -1,5 +1,5 @@
 """
-https://leetcode.com/problems/patching-array/submissions/1290233729
+https://leetcode.com/problems/patching-array/submissions/1290242242
 
 Time Complexity: O(n)
 Space Complexity: O(1)
@@ -9,15 +9,16 @@ Type: Greedy
 class Solution:
     def minPatches(self, nums: List[int], n: int) -> int:
         count = 0
-        missed = 1
-        i = 0
-
-        while missed <= n:
-            if i < len(nums) and nums[i] <= missed:
-                missed += nums[i]
-                i += 1
-            else:
-                missed += missed
+        right = 0
+        for num in nums:
+            while n > right and num > right + 1:
+                right += right + 1
                 count += 1
-        return count
+            right += num
+        
+        while n > right:
+            right += right + 1
+            count += 1
+
+        return count 
             
